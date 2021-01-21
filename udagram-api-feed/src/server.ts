edd306e5c75f7041496547e6d3e12688d45402d0
Aishwarya Manicka <manickaa@oregonstate.edu>
2021-01-20 22:30:07 -0800

    Changes

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -12,12 +12,12 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
 (async () => {
   await sequelize.addModels(V0_FEED_MODELS);
   await sequelize.sync();
-
+  console.log("Hii")
   const app = express();
   const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
-
+  console.log("Help")
   app.use(cors({
     allowedHeaders: [
       'Origin', 'X-Requested-With',
