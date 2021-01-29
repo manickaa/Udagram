bee091f0b2d3026ca5b4997f02cbacf0977497e8
Aishwarya Manicka <manickaa@oregonstate.edu>
2021-01-28 23:20:42 -0800

    Updated apiHost to external ip of reverseproxy service

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'ab46095b1caa1467cb87ac67a4507bbe-581683941.us-east-1.elb.amazonaws.com'
 };
 
 /*
